d39fd7aca05152dc3513d230dbf768c5237bc0c7
overtorment <overtorment@gmail.com>
2026-08-25 09:00:22 +0100

    Pin echalote so Android can fetch Tor directories over HTTP.
    HttpURLConnection failed every consensus/microdesc download, so exit circuits never built. The pin includes the TCP client and the Content-Length stop so keep-alive DirPorts cannot time out a finished response.

--- peers/src/commonMain/kotlin/io/bluewallet/blueberry/peers/Log.kt
+++ peers/src/commonMain/kotlin/io/bluewallet/blueberry/peers/Log.kt
@@ -1,5 +1,18 @@
 package io.bluewallet.blueberry.peers
 
-fun log(scope: String, message: String) {}
+fun log(scope: String, message: String) {
+    if (!logged(scope)) return
+    println("blueberry[$scope] $message")
+}
 
-fun logError(scope: String, message: String, err: Throwable? = null) {}
+fun logError(scope: String, message: String, err: Throwable? = null) {
+    if (!logged(scope)) return
+    if (err == null) {
+        println("blueberry[$scope] ERROR $message")
+        return
+    }
+    println("blueberry[$scope] ERROR $message: ${err.message ?: err}")
+    err.printStackTrace()
+}
+
+private fun logged(scope: String): Boolean = scope == "broadcast" || scope == "tor"
